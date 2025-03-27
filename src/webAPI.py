1e2d5c7026075083cd2b41f06d523086b9080e76
Arun Sisarran <arunsisarran@gmail.com>
2025-03-27 15:02:17 -0400

    refactor of the model.py code

--- src/webAPI.py
+++ src/webAPI.py
@@ -1,1 +1,18 @@
+from flask import Flask, Response, jsonify
+import cv2
+import mediapipe as mp
+import pickle
+import numpy as np
+import createDataset
 
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def test():
+    return "Yes"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
